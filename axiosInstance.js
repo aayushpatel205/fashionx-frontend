bffd989b99652d29246da5dc62c70224a4b4f402
Aayush <aayush2052005@gmail.com>
2025-05-13 19:11:46 +0530

    first

--- axiosInstance.js
+++ axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const PORT = 8002;
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:${PORT}`, // Replace with your backend URL
+  baseURL: import.meta.env.VITE_BACKEND_URL, // Replace with your backend URL
   withCredentials: true, // Ensures cookies are sent with requests
 });
 
